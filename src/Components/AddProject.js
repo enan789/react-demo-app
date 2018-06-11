f14e92100ad9c85ceff3d8b0c20f272434d2ebfd
enan789 <enan7890@gmail.com>
2018-06-11 13:28:09 -0400

    created components

---
+++ src/Components/AddProject.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
+
+class AddProject extends Component {
+  static defaultProps = {
+    categories: [
+      'Web Design', 'Web Development', 'Mobile Development'
+    ]
+  }
+
+  handleSubmit(e){
+    console.log('submitted');
+    e.preventDefault();
+  }
+
+  render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value="category">{category}</option>
+    });
+
+    return (
+      <div className="AddProject">
+        <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Title</label><br/>
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label>Category</label><br/>
+            <select ref="category">
+              {categoryOptions}
+            </select>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
